5f0917d2cc15075fcb64182ea39722a927da02de
Matthew Bridger <matthewbridger2000@gmail.com>
2024-05-26 20:14:37 +0100

    Updated user controller and model.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -48,3 +48,81 @@ exports.addUser = async (req, res, next) => {
         res.send(error);
     }
 };
+
+// Edit User
+exports.editUser = async (req, res, next) => {
+    try {
+        // New User
+        const newUser = { ...req.body };
+
+        // Existing User
+        let currUser = await User.findByPk(newUser.id);
+
+        // User
+        currUser.firstName = newUser.firstName;
+        currUser.lastName = newUser.lastName;
+        currUser.email = newUser.email;
+
+        // Update row in database
+        await currUser.save();
+
+        // Return result
+        res.status(200);
+        res.send(currUser);
+    }
+    catch (error) {
+        res.status(400);
+        res.send(error);
+    }
+};
+
+// Restore User using param key
+exports.restoreUser = async (req, res, next) => {
+    try {
+        let currentUser = await User.findByPk(req.params.key);
+        
+        currentUser.isDeleted = false;
+
+        await currentUser.save();
+
+        res.status(200);
+        res.send(currentUser);
+    }
+    catch (error) {
+        res.status(400);
+        res.send(error);
+    }
+};
+
+// Soft delete User using param key
+exports.deleteUser = async (req, res, next) => {
+    try {
+        let currentUser = await User.findByPk(req.params.key);
+        
+        currentUser.isDeleted = true;
+
+        await currentUser.save();
+
+        res.status(200);
+        res.send(currentUser);
+    }
+    catch (error) {
+        res.status(400);
+        res.send(error);
+    }
+};
+
+// Hard delete User using param key
+exports.hardDeleteUser = async (req, res, next) => {
+    try {
+        const currentUser = await User.findByPk(req.params.key);
+
+        await currentUser.destroy();
+        
+        res.send({ success: true });
+    }
+    catch (error) {
+        res.status(400);
+        res.send(error);
+    }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -16,5 +16,17 @@ router.get('/:key', UserController.getUser);
 // Add
 router.post('/add', UserController.addUser);
 
+// Edit
+router.put('/edit', UserController.editUser);
+
+// Restore
+router.post('/restore/:key', UserController.restoreUser);
+
+// Soft Delete
+router.delete('/delete/:key', UserController.deleteUser);
+
+// Hard Delete
+router.delete('/hard-delete/:key', UserController.hardDeleteUser);
+
 // Export the router
 module.exports = router;
